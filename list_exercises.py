5760a246652a9680c671a933a72208d184198995
rotem <rotembenhamo92@gmail.com>
2021-01-31 20:32:01 +0200

    add more list function

--- list_exercises.py
+++ list_exercises.py
@@ -31,6 +31,19 @@ def get_min(_list):
     return min
 
 
+def remove_dup(_list):
+    return list(set(_list))
+
+
+def get_diff(l1,l2):
+    # result = []
+    # for i in l1:
+    #     if i not in l2:
+    #         result.append(i)
+    # return result
+    return list(set(l1)-set(l2))
+
+
 if __name__ == '__main__':
     print(sum_list([1, 6, 4]))
     print(multiply_list([1, 6, 4, 5]))
@@ -38,3 +51,5 @@ if __name__ == '__main__':
         get_max([1, 6, 4, 5, 35, 12, 43, 23, 56, 78, 54, 34, 56, 76, 34, 234, 23454, 12, 34, 55, 34, 223]))
     print(
         get_min([16, 14, 56, 78, 54, 34, 56, 76, 34, 234, 23454, 12, 34, 55, 34, 223]))
+    print(remove_dup([1,2,3,3,3,3,2,3,2,3,1,1,4]))
+    print(get_diff([1,2,3,6],[2,4,5]))
